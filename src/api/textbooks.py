d1ad251f42ad22b32cec721f209718692265b62e
IvanA673 <22ivana04@gmail.com>
2025-05-04 19:21:38 -0700

    created get textbook by idendpoint.

--- src/api/textbooks.py
+++ src/api/textbooks.py
@@ -10,6 +10,12 @@ router = APIRouter(
     dependencies=[Depends(auth.get_api_key)],
 )
 
+class Textbook(BaseModel):
+    id: int
+    title: str
+    author: str
+    edition: str
+
 @router.get("/test-db-connection")
 def test_db_connection():
     # Attempt to connect to the database
@@ -21,3 +27,27 @@ def test_db_connection():
 @router.get("/")
 def get_textbooks():
     return {"message": "textbooks returned successfully."}
+
+@router.get("/textbooks/{textBookId}")
+def get_textbook_by_id(textBookId: int):
+    with db.engine.connect() as connection:
+        result = connection.execute(
+            sqlalchemy.text(
+                """
+                SELECT 
+                    id, title, author, edition 
+                FROM 
+                    textbooks 
+                WHERE 
+                    id = :id
+                """), 
+                {"id": textBookId}
+            ).fetchone()
+        if result is None:
+            return {"message": f"textbook with id {textBookId} not found."}
+        return Textbook(
+            id=result.id,
+            title=result.title,
+            author=result.author,
+            edition=result.edition
+        )
