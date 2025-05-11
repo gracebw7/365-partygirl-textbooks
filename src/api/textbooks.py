72ce2c24e3495ab39cc217df5b116034b31a743f
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-05-11 16:30:09 -0700

    feat: added endpoint for all info to be added to textbook

--- src/api/textbooks.py
+++ src/api/textbooks.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends, status
 from pydantic import BaseModel, Field
 import sqlalchemy
-from src.api import auth
+from src.api import auth, professors, classes, courses, classbooks, link as links
 from src import database as db
+
 
 router = APIRouter(
     prefix="/textbooks",
@@ -24,9 +25,47 @@ def test_db_connection():
         result = connection.execute(sqlalchemy.text("SELECT 1")).scalar()
         return {"status": "success", "result": result}
 
+
 @router.get("/")
 def get_textbooks():
     return {"message": "textbooks returned successfully."}
+
+class TextbookInputResponse(BaseModel):
+    textbook_id: int
+    professor_id: int
+    course_id: int
+    class_id: int
+    classbook_id: int
+    link_id: int
+
+class TextbookInput(BaseModel):
+    title: str
+    author: str
+    edition: str
+    prof_first: str
+    prof_last: str
+    department: str
+    course_number: int
+    url: str
+
+@router.post("/all_info", response_model=TextbookInputResponse)
+def add_textbook_info(input: TextbookInput):
+    professor = professors.create_get_professor(professors.Professor(first=input.prof_first, last=input.prof_last))
+    course = courses.create_get_course(courses.Course(department=input.department, number=input.course_number))
+    class_ = classes.create_get_class(classes.Class(department=input.department, number=input.course_number, prof_first=input.prof_first, prof_last=input.prof_last))
+    textbook = create_get_textbook(Textbook(title=input.title, author=input.author, edition=input.edition))
+    classbook = classbooks.create_classbook(class_id=class_.class_id, book_id=textbook.textbook_id)
+    link = links.create_link(textbook_id=textbook.textbook_id, url=input.url)
+
+    return TextbookInputResponse(
+        textbook_id=textbook.textbook_id,
+        professor_id=professor.prof_id,
+        course_id=course.course_id,
+        class_id=class_.class_id,
+        classbook_id=classbook.classbook_id,
+        link_id=link.link_id
+    )
+
 
 @router.get("/textbooks/{textBookId}")
 def get_textbook_by_id(textBookId: int):
@@ -76,7 +115,7 @@ def create_textbook(title: str, author: str, edition: str):
 
 #attempts to find a textbook with the given attributes, otherwise it creates one
 @router.post("/", response_model=TextbookIdResponse)
-def create_get_professor(text_request:Textbook):
+def create_get_textbook(text_request:Textbook):
 
     with db.engine.begin() as connection:
         ret_id = connection.execute(
@@ -91,13 +130,13 @@ def create_get_professor(text_request:Textbook):
         ).scalar_one_or_none()
 
         if ret_id is not None:
-            return TextbookIdResponse(text_id = ret_id)
+            return TextbookIdResponse(textbook_id=ret_id)
 
     with db.engine.begin() as connection:
         ret_id = connection.execute(
                 sqlalchemy.text(
                     """
-                    INSERT INTO textbookd (title, author, edition)
+                    INSERT INTO textbooks (title, author, edition)
                     VALUES (:title, :author, :edition)
                     RETURNING id
                     """
@@ -105,7 +144,9 @@ def create_get_professor(text_request:Textbook):
                 {"title":text_request.title,"author":text_request.author,"edition":text_request.edition},
             ).scalar_one()
     
-    return TextbookIdResponse(text_id = ret_id)
+    return TextbookIdResponse(textbook_id=ret_id)
+
+
 
 
 class TextbookLinks(BaseModel):
