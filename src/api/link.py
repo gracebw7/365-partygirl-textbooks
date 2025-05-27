3b1d404f02ac7b33a7ed8dad7643b166dde5190d
Grace Boye-Williams <gboyewilliams@gmail.com>
2025-05-27 13:33:52 -0700

    feat: added concurrency protection for links

--- src/api/link.py
+++ src/api/link.py
@@ -59,34 +59,40 @@ def get_link_by_id(link_id: int):
     
 @router.post("/", response_model=LinkIdResponse)
 def create_link(link:Link):
-    with db.engine.begin() as connection:
-        # Check if the textbook exists
-        result = connection.execute(
-            sqlalchemy.text(
-                """
-                SELECT id FROM textbooks WHERE id = :textbook_id
-                """
-            ),
-            {"textbook_id": link.textbook_id}
-        ).fetchone()
-        if result is None:
-            raise HTTPException(
-                status_code=404,
-                detail=f"Textbook with id {link.textbook_id} not found."
-            )
-        
-        ret_id = connection.execute(
-            sqlalchemy.text(
-                """
-                INSERT INTO links (textbook_id, url)
-                VALUES (:textbook_id, :url)
-                RETURNING id
-                """
-            ),
-            {"textbook_id": link.textbook_id, "url": link.url},
-        ).scalar_one()
+    try:
+        with db.engine.begin() as connection:
+            # Check if the textbook exists
+            result = connection.execute(
+                sqlalchemy.text(
+                    """
+                    SELECT id FROM textbooks WHERE id = :textbook_id
+                    """
+                ),
+                {"textbook_id": link.textbook_id}
+            ).fetchone()
+            if result is None:
+                raise HTTPException(
+                    status_code=404,
+                    detail=f"Textbook with id {link.textbook_id} not found."
+                )
+            
+            ret_id = connection.execute(
+                sqlalchemy.text(
+                    """
+                    INSERT INTO links (textbook_id, url)
+                    VALUES (:textbook_id, :url)
+                    RETURNING id
+                    """
+                ),
+                {"textbook_id": link.textbook_id, "url": link.url},
+            ).scalar_one()
 
-    return LinkIdResponse(link_id=ret_id)
+        return LinkIdResponse(link_id=ret_id)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Unexpected error: {str(e)}"
+        )
 
 @router.post("/{link_id}")
 def request_deletion(link_req: DeleteLinkRequest):
@@ -124,7 +130,7 @@ def delete_link(link_id: int):
         result = connection.execute(
             sqlalchemy.text(
                 """
-                SELECT id FROM links WHERE id = :link_id
+                SELECT id for update FROM links WHERE id = :link_id
                 """
             ),
             {"link_id": link_id}
